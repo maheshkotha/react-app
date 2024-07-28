c9012e575c861c33cb77257909f58665ddfc7071
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2024-07-28 18:33:17 +0530

    Test (#15)
    * html filed and linked script file
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: Kotha Mahesh Babu <Mahesh.Kotha@dazn.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from 'react-dom/client'
+import React from 'react';
 
 const App = () => {
-    return (<div>
-        <h1>Raact app Updated....</h1>
-    </div>)
-}
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App />)
-
+export default App;
